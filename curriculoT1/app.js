f0e7ab3141be1b99f63a0c0527e4fab8cf65ece5
felippe1987 <149263769+felippe1987@users.noreply.github.com>
2024-07-15 21:19:25 -0300

    Add files via upload

---
+++ curriculoT1/app.js
@@ -0,0 +1,15 @@
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark_theme')
+
+    var className = document.body.className;
+    if(className == "light_theme"){
+        this.textContent = 'Dark';
+    }
+    else{
+        this.textContent = 'Light';
+    }
+    console.log('current classe name:' + className)
+});
+
